d824af66b39797192f7256677c9fa857267b2675
The Sweeper Monk <157958612+voideye0@users.noreply.github.com>
2024-07-07 18:18:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// app.js
 const express = require('express');
+const ngrok = require('ngrok');
 const app = express();
 const port = 3000;
 
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`App listening at http://localhost:${port}`);
+  const url = await ngrok.connect({ addr: port, authtoken: process.env.NGROK_TOKEN });
+  console.log(`ngrok tunnel opened at: ${url}`);
 });
